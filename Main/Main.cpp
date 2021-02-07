a9aff04851c45d4a5acd40a5ee2427ffd40aae63
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2021-02-08 01:24:34 +0300

    Update Main.cpp

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -3,13 +3,8 @@
 #include "../Core/CRYPTON.h"
 
 int main(int argc, char** argv){
-	#ifdef __linux__
-		system("clear");
-		CRYPTON crypt;
-		crypt.run();
-		return 0;
-	#else
-		std::cout << "\n\n";
-		return 0;
-	#endif
+	system("clear");
+	CRYPTON crypt;
+	crypt.run();
+	return 0;
 }
